f530469f2ede6668308117939bfaa1a0e06c4475
viscatommy <tommy21heel@gmail.com>
2025-09-21 13:28:58 +0900

    Add existing project folder: daily-report1

---
+++ daily-report1/.prettierrc.js
@@ -0,0 +1,11 @@
+export default {
+    semi: false,
+    singleQuote: false,
+    tabWidth: 2,
+    useTab: false,
+    printWidth: 100,
+    trailingComma: "all",
+    bracketSpacing: true,
+    allowParens: "avoid",
+    endOfLine: "if"
+};
